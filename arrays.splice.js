f81c1e87d45cfd4e2a99adb70ccccd4011b2296a
Adrian Skar <adrian@adrianskar.com>
2019-09-13 20:00:21 +0200

    Done until objects

---
+++ arrays.splice.js
@@ -0,0 +1,44 @@
+/*jshint esversion: 6*/
+console.log("test");
+
+
+const chars = ["One", "two", "three", "four"];
+console.log(chars);
+
+//Remove one item from position 0 and add another instead
+chars.splice(0, 1, "five");
+console.log(chars);
+
+//Remove two from the middle and add one in its place
+chars.splice(1, 2, "six");
+console.log(chars);
+
+//Remove 'six' and add 'seven' and 'eight
+chars.splice(1, 1, 'seven', 'eight');
+console.log(chars);
+
+//Remove 'seven; and 'eight'
+chars.splice(1, 2);
+console.log(chars);
+
+//Push a couple more
+chars.push('nine', 'ten');
+console.log(chars);
+
+//Reverse items
+chars.reverse();
+console.log(chars);
+
+//Sort
+chars.sort();
+console.log(chars);
+
+//indexof
+const found = chars.indexOf("fodur");
+console.log(found);  //not found
+
+const found2 = chars.indexOf("four");
+console.log(found2);  //found at 1
+
+const found3 = chars.lastIndexOf('nine');
+console.log(found3); //the last one found if there is more than one 
---
+++ objects.js
@@ -0,0 +1,41 @@
+/*jshint esversion: 6*/
+
+const dog = {
+  'name': "Woody",
+  'type': "dog"
+};
+const cat = {
+  'name': "Bob",
+  'type': "cat"
+};
+console.log(dog);
+
+const petNames = ["Woody", "Bob"];
+
+const pets = [dog, cat];
+
+//Add a new pet; object literal
+pets.push({ 'name': 'Roxy', 'type': 'dog' });
+console.log(pets[2]); //pets sub 2
+
+//Add properties
+cat.age = 2;
+dog['age'] = 14;
+console.log(pets);
+
+pets[0].age = 14;
+pets[2]['age'] = 6;
+console.log(pets);
+
+
+pets[1].age = 3;
+pets[1].hungry = true;
+console.log(pets);
+
+
+
+
+
+
+
+
