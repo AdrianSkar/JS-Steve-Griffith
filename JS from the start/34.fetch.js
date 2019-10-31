78f024d2faf3378bb4c004405ad76d3bd42af85c
Adrian Skar <adrian@adrianskar.com>
2019-10-31 22:08:35 +0100

    1944 - 31

--- JS from the start/34.fetch.js
+++ JS from the start/34.fetch.js
@@ -16,8 +16,24 @@ console.log('Fetch: ', uri);
 //any user id id higher than 10 will generate a 404 error
 
 fetch(uri)
-	.then()
-	.then()
-	.catch();
+	.then(function (response) {
+		if (response.status == 200) {
+			return response.json();
+		}
+		else {
+			throw new Error('Invalid user ID');
+		}
+	})
+	.then((data) => {
+		console.log(data);
+		let jsonData = JSON.stringify(data);
+
+		let output = document.getElementById('output');
+		output.textContent = jsonData;
+		console.log(jsonData);
+	})
+	.catch((err) => {
+		console.log('Error: ', err.message);
+	});
 
 
---
+++ JS from the start/35.function currying.js
@@ -0,0 +1,5 @@
+/*jshint esversion: 6*/
+
+// In JS, functions are first-class objects, just like String, Number, Boolean
+// This means that they can be passed to functions or returned from functions
+
