9388eeaba2705c6aeae38e34ccf843eefb871c95
Adrian Skar <adrian@adrianskar.com>
2019-11-08 21:43:24 +0100

    1945 - 8

--- JS from the start/41. Event bubbling and propagation.js
+++ JS from the start/41. Event bubbling and propagation.js
@@ -1,2 +1,32 @@
 /*jshint esversion: 6*/
+// element.addEventListener(type, func, useCapture);
+// Capture: from outside 
+// Bubbling: from inside (default, false)
 
+let m = document.getElementById('m');
+let d = document.getElementById('d');
+let p = document.getElementById('p');
+let s = document.getElementById('s');
+
+let highlight = (ev) => {
+	// Add CSS class gold to the clicked element
+	let target = ev.currentTarget;
+	target.className = 'gold';
+	// ev.stopPropagation(); // stops the bubbling 
+	reset(target); // resets the background class after 2secs
+};
+
+let reset = (_element) => {
+	setTimeout(() => {
+		_element.className = '';
+	}, 2000);
+}
+
+d.addEventListener('click', (ev) => {
+	// ev.stopImmediatePropagation(); //stops other elements from listening to click events from this point, including the below ones
+	console.log('Hi, I am a DIV');
+}, false);
+
+[m, d, p, s].forEach((element) => {
+	element.addEventListener('click', highlight, true);
+});
